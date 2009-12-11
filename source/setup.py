29c5df08171e6e5198d4c0c52a06159e27c8ddef
Doug Bell <doug101@bellz.org>
2009-12-11 06:51:29 -0500

    added windows dos binary for command line use

--- source/setup.py
+++ source/setup.py
@@ -2,10 +2,17 @@
 from distutils.core import setup
 import py2exe
 
-setup(name = 'treeline',
-      windows = [{'script': 'treeline.py',
-                  'icon_resources': [(1, '../win/treeline.ico')]}],
-      options = {'py2exe': {'includes': ['sip', 'urllib2'],
-                            'dist_dir': 'dist/lib'}})
+guiProg = {'script': 'treeline.py',
+           'icon_resources': [(1, '../win/treeline.ico')],
+           'dest_base': 'treeline'}
+
+consoleProg = {'script': 'treeline.py',
+               'icon_resources': [(1, '../win/treeline.ico')],
+               'dest_base': 'treeline_dos'}
+
+options = {'py2exe': {'includes': ['sip', 'urllib2'],
+                      'dist_dir': 'dist/lib'}}
+
+setup(windows=[guiProg], console=[consoleProg], options=options)
 
 # run with: python setup.py py2exe
